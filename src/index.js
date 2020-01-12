470b1253cd86a419a7ffe090e786a3e66bef48ce
Wenny Hidayat <wenny.h@live.com>
2020-01-12 15:52:58 +1100

    Finished tasks, added ranking logs

--- src/index.js
+++ src/index.js
@@ -9,16 +9,17 @@
 // Sort/accumulate IPs (is there a need to sort or just one-by-one? 
 // Rank IPs
 // Do the same for URLs
+const orderBy = require('lodash/orderBy')
+const fs = require('fs')
+const utils = require('./utils')
 
-// This can be in own const/config file
+// This can be in own const/config file or duck-typed as it is
 const regexPatterns = {
   IP: '(.+?) -',
   URL: '"GET (.+?) HTTP/1.1"'
 }
 
 // Loading file
-const fs = require('fs')
-const utils = require('./utils')
 const logContent = fs.readFileSync(process.argv[2], 'utf8')
 
 // Main cleaned up array of logs
@@ -27,16 +28,16 @@ const logArray = utils.parseLogContent(logContent)
 // • The number of unique IP addresses
 const IPArray = utils.getSubList(logArray, regexPatterns.IP)
 const uniqueIPArray = utils.getUniqueFromList(IPArray)
-// console.log('The number of unique IP addresses is', uniqueIPs.length)
+console.log('The number of unique IP addresses is', uniqueIPArray.length)
 
+// • The top 3 most active IP addresses
+// Note that top three ignores equal-occurence: so in reality it could be:
+// 3,3,3,[3,3,2,1,1,1...] or 3,2,1,[1,1,1]... 
+const sortedIPs = orderBy(utils.getOccurrences(IPArray, uniqueIPArray, 'IP'), ['occurrences'], ['desc'])
+console.log('The top 3 most active IP addresses are:\n', sortedIPs[0].IP, '\n', sortedIPs[1].IP, '\n', sortedIPs[2].IP)
+
+// • The top 3 most visited URLs
 const URLArray = utils.getSubList(logArray, regexPatterns.URL)
 const uniqueURLArray = utils.getUniqueFromList(URLArray)
-
-// console.log(uniqueIPs)
-// console.log('The number of unique URL addresses is', uniqueURLs.length)
-// console.log(uniqueURLs)
-
-const topThreeIPs = utils.countOccurences(IPArray, uniqueIPArray, 'IP')
-console.log(topThreeIPs)
-// console.log('The top 3 most active IP addresses are', uniqueIPs.length)
-// console.log('The top 3 most visited URLs are', uniqueIPs.length)
+const sortedURLs = orderBy(utils.getOccurrences(URLArray, uniqueURLArray, 'URL'), ['occurrences'], ['desc'])
+console.log('The top 3 most visited URLs are:\n', sortedURLs[0].URL, '\n', sortedURLs[1].URL, '\n', sortedURLs[2].URL)
--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,6 @@
-
-// ============= Utils start here =============== //
 /** 
  * Loader function, outputs an array of logs.
- * @param {string} pathname Pathname of the file to be loaded.
+ * @param {string} loadedLogContent Loaded string of log data to be converted into an array.
 */
 function parseLogContent(loadedLogContent) {
   // Note: this only removes [null, undefined, ] - it does not validate.
@@ -12,41 +10,53 @@ function parseLogContent(loadedLogContent) {
 }
 
 /**
- * Selects the IP part of the string.
- * @param {logArray} logArray Array containing logs containing parts to be selected.
- * @param {regexPattern} regexPattern The pattern used for matching and selection.
+ * Cleans up the array from duplicates, returns a new array of unique values.
+ * @param {Array} selectedArray Array 
  */
 function getUniqueFromList(selectedArray) {
   return Array.from(new Set(selectedArray))
 }
 
+/**
+ * Extracts part of the entries according to a pattern, returns a new array of matched values.
+ * @param {Array} logArray 
+ * @param {Array} regexPattern 
+ */
 function getSubList(logArray, regexPattern) {
   return logArray.map(log => log.match(regexPattern)[1])
 }
 
-// The drawback of this is that this only makes very specific identifier object
-// Such as [ { IP: '000.00.000.00'}, occurences: 1 } ] or [ { URL: '/something'}, occurences: 1 } ]
-// Is there a better way? Then again occurences is a shared keyword...
-// Unless we do something like "occurences" table
-// { 
-//   occurences: {
-//     IP: {
-//       '00.000.000.00': 1,
-//       '00.000.000.01': 1,
-//     }
-//     '/something': 3,
-//     '/something-else': 1,
-//   }
-// }
-// Depends on future use, that might be over-engineering though.
-
-function countOccurences(fullList, uniqueList, identifier) {
-  return uniqueList.map(item => {
-    const occurences = fullList.reduce( (accumulator, currentValue, i) => {
+/**
+ * Counts how many times something appeared by going through the reference array and looking up its values in the target array.
+ * @param {Array} targetArray The array to be matched against for counting occurrences.
+ * @param {Array} referenceArray The array to be used as reference.
+ * @param {string} identifier Label for the returned object
+ */
+function getOccurrences(targetArray, referenceArray, identifier) {
+  // The drawback of this is that this only makes very specific identifier object
+  // Such as [ { IP: '000.00.000.00'}, occurrences: 1 } ] or [ { URL: '/something'}, occurrences: 1 } ]
+  // This could be repurposed [{ IP: occurrences, uniqueness, etc }, { URL: occurrences, uniqueness }]
+  // Depends on future use, that might be over-engineering, 
+  // because there are other valid groupings, too, for example
+  // we could do something like "occurrences" table
+  // { 
+  //   occurrences: {
+  //     IPs: {
+  //       '00.000.000.00': 1,
+  //       '00.000.000.01': 1,
+  //     }
+  //     URLs: {
+  //       '/something': 3,
+  //       '/something-else': 1,
+  //     }
+  //   }
+  // }
+  return referenceArray.map(item => {
+    const occurrences = targetArray.reduce( (accumulator, currentValue, i) => {
       if (currentValue === item) return accumulator + 1
       return accumulator
     }, 0)
-    return {[identifier]: item, occurences}
+    return {[identifier]: item, occurrences}
   })
 }
 
@@ -54,5 +64,5 @@ module.exports = {
   parseLogContent,
   getSubList,
   getUniqueFromList,
-  countOccurences
+  getOccurrences
 }
